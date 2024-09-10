b7b61d0558e61985c24a545ccf8c3d6c7e6fd85e
Adebayo Temi <temiq3@gmail.com>
2024-09-10 11:39:21 +0100

    second commit

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,6 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
-import UserNameMenu from "./usernameMenu";
+import UserNameMenu from "./UserNameMenu";
 
 const MainNav = () => {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
