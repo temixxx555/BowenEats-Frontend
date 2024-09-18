e1fa7afe6ff4457e78daaa60d277488ed588e60d
Adebayo Temi <temiq3@gmail.com>
2024-09-18 18:23:13 +0100

    added search page filters

--- src/api/myUserApi.tsx
+++ src/api/myUserApi.tsx
@@ -8,7 +8,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const useGetMyUser = () => {
   const { getAccessTokenSilently } = useAuth0();
 
-  const getMyUserRequest = async (): Promise<User>=> {
+  const getMyUserRequest = async (): Promise<User> => {
     const accessToken = await getAccessTokenSilently();
 
     const response = await fetch(`${API_BASE_URL}/api/my/user`, {
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,17 @@
 import landingImage from "../assets/landing.png";
-import appLoad from  "../assets/appDownload.png"
+import appDownloadImage from "../assets/appDownload.png";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import { useNavigate } from "react-router-dom";
+
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`,
+    });
+  };
+
   return (
     <div className='flex flex-col gap-12'>
       <div className='md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16'>
@@ -8,18 +19,22 @@ const HomePage = () => {
           Tuck into a takeway today
         </h1>
         <span className='text-xl'>Food is just a click away!</span>
+        <SearchBar
+          placeHolder='Search by City or Town'
+          onSubmit={handleSearchSubmit}
+        />
       </div>
       <div className='grid md:grid-cols-2 gap-5'>
         <img src={landingImage} />
         <div className='flex flex-col items-center justify-center gap-4 text-center'>
           <span className='font-bold text-3xl tracking-tighter'>
-            order takeaway even faster
+            Order takeaway even faster!
           </span>
           <span>
-            Download the MernEats App for faster oredring and personalized
-            recommendation
+            Download the MernEats App for faster ordering and personalised
+            recommendations
           </span>
-<img src={appLoad} />
+          <img src={appDownloadImage} />
         </div>
       </div>
     </div>
