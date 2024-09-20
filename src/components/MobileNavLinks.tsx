066b64eec060a9f50241c8135c0d8b596865bf83
Adebayo Temi <temiq3@gmail.com>
2024-09-20 16:01:47 +0100

    added checkout flow

--- src/components/MobileNavLinks.tsx
+++ src/components/MobileNavLinks.tsx
@@ -7,26 +7,26 @@ const MobileNavLinks = () => {
   return (
     <>
       <Link
-        to="/order-status"
-        className="flex bg-white items-center font-bold hover:text-orange-500"
+        to='/order-status'
+        className='flex bg-white items-center font-bold hover:text-orange-500'
       >
         Order Status
       </Link>
       <Link
-        to="/manage-restaurant"
-        className="flex bg-white items-center font-bold hover:text-orange-500"
+        to='/manage-restaurant'
+        className='flex bg-white items-center font-bold hover:text-orange-500'
       >
         My Restaurant
       </Link>
       <Link
-        to="/user-profile"
-        className="flex bg-white items-center font-bold hover:text-orange-500"
+        to='/user-profile'
+        className='flex bg-white items-center font-bold hover:text-orange-500'
       >
         User Profile
       </Link>
       <Button
         onClick={() => logout()}
-        className="flex items-center px-3 font-bold hover:bg-gray-500"
+        className='flex items-center px-3 font-bold hover:bg-gray-500'
       >
         Log Out
       </Button>
--- src/forms/user-profile-form/UserProfileForm.tsx
+++ src/forms/user-profile-form/UserProfileForm.tsx
@@ -1,5 +1,6 @@
-import LoadingButton from "@/components/LoadingButton";
-import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Form,
   FormControl,
@@ -10,11 +11,10 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import LoadingButton from "@/components/LoadingButton";
+import { Button } from "@/components/ui/button";
 import { User } from "@/types";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { useEffect } from "react";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
 
 const formSchema = z.object({
   email: z.string().optional(),
@@ -24,42 +24,52 @@ const formSchema = z.object({
   country: z.string().min(1, "Country is required"),
 });
 
-type UserFormData = z.infer<typeof formSchema>;
+export type UserFormData = z.infer<typeof formSchema>;
 
 type Props = {
+  currentUser: User;
   onSave: (userProfileData: UserFormData) => void;
   isLoading: boolean;
-  currentUser: User;
+  title?: string;
+  buttonText?: string;
 };
-const UserProfileForm = ({ onSave, isLoading, currentUser }: Props) => {
+
+const UserProfileForm = ({
+  onSave,
+  isLoading,
+  currentUser,
+  title = "User Profile",
+  buttonText = "Submit",
+}: Props) => {
   const form = useForm<UserFormData>({
     resolver: zodResolver(formSchema),
     defaultValues: currentUser,
   });
-  useEffect(()=>{
-    form.reset(currentUser)
-  },[currentUser,form])
+
+  useEffect(() => {
+    form.reset(currentUser);
+  }, [currentUser, form]);
+
   return (
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSave)}
-        className='space-y-4 bg-gray-50 rounded-lg md:p-10'
+        className="space-y-4 bg-gray-50 rounded-lg md:p-10"
       >
         <div>
-          <h2 className='text-2xl font-bold'>User Profile Form</h2>
+          <h2 className="text-2xl font-bold">{title}</h2>
           <FormDescription>
             View and change your profile information here
           </FormDescription>
         </div>
-
         <FormField
           control={form.control}
-          name='email'
+          name="email"
           render={({ field }) => (
             <FormItem>
               <FormLabel>Email</FormLabel>
               <FormControl>
-                <Input {...field} disabled className='bg-white' />
+                <Input {...field} disabled className="bg-white" />
               </FormControl>
             </FormItem>
           )}
@@ -67,27 +77,27 @@ const UserProfileForm = ({ onSave, isLoading, currentUser }: Props) => {
 
         <FormField
           control={form.control}
-          name='name'
+          name="name"
           render={({ field }) => (
             <FormItem>
               <FormLabel>Name</FormLabel>
               <FormControl>
-                <Input {...field} className='bg-white' />
+                <Input {...field} className="bg-white" />
               </FormControl>
               <FormMessage />
             </FormItem>
           )}
         />
 
-        <div className='flex flex-col md:flex-row gap-4'>
+        <div className="flex flex-col md:flex-row gap-4">
           <FormField
             control={form.control}
-            name='addressLine1'
+            name="addressLine1"
             render={({ field }) => (
-              <FormItem className='flex-1'>
+              <FormItem className="flex-1">
                 <FormLabel>Address Line 1</FormLabel>
                 <FormControl>
-                  <Input {...field} className='bg-white' />
+                  <Input {...field} className="bg-white" />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -95,12 +105,12 @@ const UserProfileForm = ({ onSave, isLoading, currentUser }: Props) => {
           />
           <FormField
             control={form.control}
-            name='city'
+            name="city"
             render={({ field }) => (
-              <FormItem className='flex-1'>
+              <FormItem className="flex-1">
                 <FormLabel>City</FormLabel>
                 <FormControl>
-                  <Input {...field} className='bg-white' />
+                  <Input {...field} className="bg-white" />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -108,12 +118,12 @@ const UserProfileForm = ({ onSave, isLoading, currentUser }: Props) => {
           />
           <FormField
             control={form.control}
-            name='country'
+            name="country"
             render={({ field }) => (
-              <FormItem className='flex-1'>
+              <FormItem className="flex-1">
                 <FormLabel>Country</FormLabel>
                 <FormControl>
-                  <Input {...field} className='bg-white' />
+                  <Input {...field} className="bg-white" />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -123,12 +133,13 @@ const UserProfileForm = ({ onSave, isLoading, currentUser }: Props) => {
         {isLoading ? (
           <LoadingButton />
         ) : (
-          <Button type='submit' className='bg-orange-500'>
-            Submit
+          <Button type="submit" className="bg-orange-500">
+            {buttonText}
           </Button>
         )}
       </form>
     </Form>
   );
 };
+
 export default UserProfileForm;
