16c83245f93d65c8f349e0bd6bdb21ab0dcdf9d9
Adebayo Temi <temiq3@gmail.com>
2024-09-28 14:02:12 +0100

    Add Vercel Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react"; 
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
@@ -24,6 +25,7 @@ createRoot(document.getElementById("root")!).render(
           <AppRoutes />
           <Toaster visibleToasts={1} position="top-right" richColors />
           <Analytics />
+          <SpeedInsights />
         </Auth0ProviderWithNavigate>
       </QueryClientProvider>
     </Router>
