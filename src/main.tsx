1648e697c5202f253241d8387a814f762df43c15
Adebayo Temi <temiq3@gmail.com>
2024-09-28 13:52:42 +0100

    Add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import { Analytics } from "@vercel/analytics/react"
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
@@ -23,6 +23,7 @@ createRoot(document.getElementById("root")!).render(
         <Auth0ProviderWithNavigate>
           <AppRoutes />
           <Toaster visibleToasts={1} position="top-right" richColors />
+          <Analytics />
         </Auth0ProviderWithNavigate>
       </QueryClientProvider>
     </Router>
